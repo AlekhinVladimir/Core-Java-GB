55f8eb16a9be81c855f62ed6e50d938ace0a575e
AlekhinVladimir <Alehin.v.m@mail.com>
2023-11-04 22:57:33 +0400

    Переделка, иде шка тупит

---
+++ HW/HW3/Task1/src/main/java/Employee.java
@@ -0,0 +1,35 @@
+public class Employee {
+    private int year;
+    private int month;
+    private int day;
+
+    public Employee(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    // Метод компаратора для сравнения двух дат
+    // Возвращает:
+    // -1, если первая дата меньше второй;
+    // 0, если даты равны;
+    // 1, если первая дата больше второй;
+    public int compareDates(Employee other) {
+        int diffYears = this.year - other.year;
+        int diffMonths = this.month - other.month;
+        int diffDays = this.day - other.day;
+
+        // Сравниваем года
+        if (diffYears != 0) {
+            return Integer.compare(diffYears, 0);
+        }
+
+        // Сравниваем месяцы
+        if (diffMonths != 0) {
+            return Integer.compare(diffMonths, 0);
+        }
+
+        // Сравниваем дни
+        return Integer.compare(diffDays, 0);
+    }
+}
---
+++ HW/HW3/Task2/src/main/java/Manager.java
@@ -0,0 +1,16 @@
+// Класс-наследник "Руководитель"
+public class Manager extends Employee {
+
+    public Manager(String name, double salary) {
+        super(name, salary);
+    }
+
+    // Cтатический метод повышения зарплаты для всех сотрудников, если они не являются руководителями
+    public static void increaseSalary(Employee[] employees, double percentage) {
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                employee.setSalary(employee.getSalary() + employee.getSalary() * percentage / 100);
+            }
+        }
+    }
+}
